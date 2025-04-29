ffc72933dd98a67ef1c531e4729fd8074d8e4b10
dogantunahan <tunahan.dogan.dev@gmail.com>
2025-04-29 18:52:49 +0300

    01:Setup

---
+++ .history/src/app/page_20250429184523.tsx
@@ -0,0 +1,7 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  ;<div>
+    <Button variant="destructive">Hello World</Button>
+  </div>
+}
---
+++ .history/src/app/page_20250429184549.tsx
@@ -0,0 +1,8 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  ;<div>
+    <Button variant="destructive">Hello World</Button>
+    sa as
+  </div>
+}
